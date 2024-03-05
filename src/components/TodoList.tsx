4b9f530d0fcf8dccc3cdf5a5db42f2013b92ef64
Finn Antonczyk <finn.antonczyk@gmail.com>
2024-03-05 19:57:56 +0100

    redux: add todoDone and toggleTodo, also btn style

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,19 +1,58 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../state/store";
+import "./style.css";
+import { useDispatch } from "react-redux";
+import { TodoState, addTodo, toggleTodo } from "../state/todo/todoSlice";
 
 function TodoList() {
   const todos = useSelector((state: RootState) => state.todo);
+  const dispatch = useDispatch();
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const todoInput = event.currentTarget.todo;
+
+    const todoTitle = todoInput.value.trim();
+    if (todoTitle) {
+      dispatch(
+        addTodo({ title: todoTitle, completed: false, id: Math.random() })
+      );
+      todoInput.value = "";
+    }
+  }
+
+  const handleSetDone = (todo: TodoState) => {
+    console.log(todo);
+    dispatch(toggleTodo(todo.id));
+  };
 
   return (
     <div>
+      <h1>Add Todo:</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="todo">Todo</label>
+          <input type="text" id="todo" />
+        </div>
+        <button type="submit" className="btn">
+          Add Todo
+        </button>
+      </form>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.title} | Completed:{" "}
-            {todo.completed ? <span>&#10004;</span> : <span>&#10008;</span>}
-          </li>
+          <>
+            <li key={todo.id + todo.title}>
+              {todo.title} | Completed:{" "}
+              {todo.completed ? <span>&#10004;</span> : <span>&#10008;</span>}
+            </li>
+            <button onClick={() => handleSetDone(todo)} className="btn">
+              {todo.completed ? "Undo" : "Done"}
+            </button>
+          </>
         ))}
       </ul>
+      <button className="btn">Show Tasks i have done</button>
+      <button className="btn">Show Task i need to do</button>
     </div>
   );
 }
--- src/state/todo/todoSlice.ts
+++ src/state/todo/todoSlice.ts
@@ -1,30 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface TodoState {
+export interface TodoState {
   id: number;
   title: string;
   completed: boolean;
 }
 
 const initialState: TodoState[] = [
-  { id: 1, title: "todo1", completed: false },
-  { id: 2, title: "todo2", completed: false },
-  { id: 3, title: "todo3", completed: false },
+  { id: 1, title: "Eat Salad", completed: false },
+  { id: 2, title: "Go Jogging", completed: false },
+  { id: 3, title: "Drink Red Bull", completed: true },
 ];
 
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
   reducers: {
-    addTodo: (state, action) => {
+    addTodo: (state, action: PayloadAction<TodoState>) => {
       state.push(action.payload);
     },
-    deleteTodo: (state, action) => {
+    deleteTodo: (state, action: PayloadAction<number>) => {
       return state.filter((todo) => todo.id !== action.payload);
+    },
+    toggleTodo(state, action: PayloadAction<number>) {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
     },
   },
 });
 
 export default todoSlice.reducer;
 
-export const { addTodo, deleteTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, toggleTodo } = todoSlice.actions;
